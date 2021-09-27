d9b493faf23a3eec497ecde5b9ff51380e12fdab
ehsan2793 <ehsan2793@gmail.com>
2021-09-27 14:32:01 -0700

    all test are passing "

--- api/server.js
+++ api/server.js
@@ -1,6 +1,6 @@
 // BUILD YOUR SERVER HERE
 const express = require('express');
-const { find, findById, insert } = require('./users/model');
+const { find, findById, insert, update, remove } = require('./users/model');
 const server = express();
 
 server.use(express.json());
@@ -47,11 +47,50 @@ server.post('/api/users', async (req, res) => {
             res.status(201).json(user);
         }
     } catch (error) {
+        res.status(500).json({
+            message: 'There was an error while saving the user to the database',
+        });
+    }
+});
+
+server.delete('/api/users/:id', async (req, res) => {
+    const id = req.params.id;
+    const removedName = await remove(id);
+    try {
+        if (!removedName) {
+            res
+                .status(404)
+                .json({ message: 'The user with the specified ID does not exist' });
+        } else {
+            res.status(200).json(removedName);
+        }
+    } catch (error) {
+        res.status(500).json({ message: 'The user could not be removed' });
+    }
+});
+
+server.put('/api/users/:id', async (req, res) => {
+    const changes = req.body;
+
+    const { id } = req.params;
+    const updatedUser = await findById(id);
+    const updates = await update(id, changes);
+    try {
+        if (!updatedUser) {
+            res
+                .status(404)
+                .json({ message: 'The user with the specified ID does not exist' });
+        } else if (!changes.name || !changes.bio) {
+            res
+                .status(400)
+                .json({ message: 'Please provide name and bio for the user' });
+        } else {
+            res.status(200).json(updates);
+        }
+    } catch (error) {
         res
             .status(500)
-            .json({
-                message: 'There was an error while saving the user to the database',
-            });
+            .json({ message: 'The user information could not be modified' });
     }
 });
 
